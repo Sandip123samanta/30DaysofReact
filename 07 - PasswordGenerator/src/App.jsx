0ed46206e112b50cc11bf0999a591f4fef3e4c5e
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2023-10-20 22:46:46 +0530

    Added a password generator project

---
+++ 07 - PasswordGenerator/src/App.jsx
@@ -0,0 +1,113 @@
+import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  return (
+    <>
+      <div className="container w-screen h-screen flex flex-col items-center justify-between bg-slate-900">
+        <div className="header bg-slate-950 w-screen h-20 flex justify-center items-center">
+          <p className='text-slate-50 text-3xl'>Password Generator</p>
+        </div>
+        <div className="w-2/5 h-2/5 shadow-md rounded-lg bg-gray-800 text-orange-500 flex flex-col items-center justify-around">
+      <h1 className='text-white text-center text-2xl my-3'>Get Your Password Here</h1>
+    <div className="flex shadow w-4/5 h-1/5 rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none text-xl w-4/5 py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white w-1/5 px-3 py-0.5 text-xl font-bold shrink-0'
+        >copy</button>
+        
+    </div>
+    <div className='flex items-center justify-around w-full h-1/5 text-xl gap-x-2'>
+      <div className='flex items-center w-2/5  gap-x-2'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer w-2/5 h-3 bg-gray-600 rounded-lg appearance-none range-lg dark:bg-gray-700'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+        </div>
+        <div className='footer w-screen h-20 bg-slate-950 flex items-center justify-center flex-col'>
+          <p className='text-slate-50'> Code with ❤️ by 
+            <a href='https://github.com/Sandip123samanta' className='text-blue-500 font-medium'>Sandip Samanta</a>
+          </p>
+          <p>
+            <a href='https://github.com/Sandip123samanta/30DaysofReact' className='text-blue-500 font-medium'>#30DaysofReact</a>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
